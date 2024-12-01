8a9a75074b6ca4cc0f549e0724da023630b62a57
Aalaap Dey <aalaapdey@gmail.com>
2024-12-01 13:50:34 +0000

    feat: implement part two of day 1

--- day-1/main.py
+++ day-1/main.py
@@ -2,3 +2,9 @@
 with open('input.txt', 'r') as file:
     print(sum(abs(int(a) - int(b)) for a, b in zip(*[sorted(list(l)) for l in list(zip(*[l[:-1].split('   ') for l in file.readlines()]))])))
 
+# Part Two
+from collections import Counter
+with open('input.txt', 'r') as file:
+    l, r = list(zip(*[l[:-1].split('   ') for l in file.readlines()]))
+    freq = Counter(r)
+    print(sum(int(n)*freq[n] for n in l))
