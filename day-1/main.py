e64c0d512ef82cea5a9f9f3c0db6bb3f8fab46f2
Aalaap Dey <aalaapdey@gmail.com>
2024-12-01 14:00:25 +0000

    refactor: minimise loc

--- day-1/main.py
+++ day-1/main.py
@@ -1,10 +1,7 @@
 # Part One
-with open('input.txt', 'r') as file:
-    print(sum(abs(int(a) - int(b)) for a, b in zip(*[sorted(list(l)) for l in list(zip(*[l[:-1].split('   ') for l in file.readlines()]))])))
+print(sum(abs(int(a) - int(b)) for a, b in zip(*[sorted(list(l)) for l in list(zip(*[l[:-1].split('   ') for l in open('input.txt', 'r')]))])))
 
 # Part Two
 from collections import Counter
-with open('input.txt', 'r') as file:
-    l, r = list(zip(*[l[:-1].split('   ') for l in file.readlines()]))
-    freq = Counter(r)
-    print(sum(int(n)*freq[n] for n in l))
+l, r = list(zip(*[l[:-1].split('   ') for l in open('input.txt', 'r')]))
+print(sum(int(n)*Counter(r)[n] for n in l))
