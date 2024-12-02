73ddbe0ba06fd4a2e0e55c97c8838c4e69abd9ca
Aalaap Dey <aalaapdey@gmail.com>
2024-12-02 14:22:03 +0000

    refactor: one-line day 1

--- day-1/main.py
+++ day-1/main.py
@@ -1,7 +1,5 @@
 # Part One
-print(sum(abs(int(a) - int(b)) for a, b in zip(*[sorted(list(l)) for l in list(zip(*[l[:-1].split('   ') for l in open('input.txt', 'r')]))])))
+print(sum(abs(int(a) - int(b)) for a, b in zip(*[sorted(list(l)) for l in list(zip(*[l[:-1].split('   ') for l in open('input.txt')]))])))
 
 # Part Two
-from collections import Counter
-l, r = list(zip(*[l[:-1].split('   ') for l in open('input.txt', 'r')]))
-print(sum(int(n)*Counter(r)[n] for n in l))
+print(sum(int(n)*__import__("collections").Counter(list(zip(*[l[:-1].split('   ')for l in open('input.txt','r')]))[1])[n]for n in list(zip(*[l[:-1].split('   ')for l in open('input.txt','r')]))[0]))
